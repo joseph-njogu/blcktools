b59e90cb09be82d4c0ae618930d9455da948a9b4
joseph-njogu <josephnjogu487@gmail.com>
2021-02-03 23:16:55 +0300

    OAuth active
    Activated auth2.

--- Cryptotrade/Cryptotrade/settings.py
+++ Cryptotrade/Cryptotrade/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
 
-    
+    'social_django',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'Cryptotrade.urls'
@@ -74,10 +76,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'Cryptotrade.wsgi.application'
 
@@ -141,12 +147,42 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated', )
 }
-AUTHENTICATION_BACKENDS = (
-        'django.contrib.auth.backends.ModelBackend',
+# AUTHENTICATION_BACKENDS = (
+#         'django.contrib.auth.backends.ModelBackend',
+#         'social_core.backends.google.GoogleOAuth2',
 
-        'allauth.account.auth_backends.AuthenticationBackend',
+#         'allauth.account.auth_backends.AuthenticationBackend',
+# )
+#setting up authentication
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.google.GoogleOAuth',
+
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "955388973949-udlln0p3t1g6fmk082prs3gu2va8dju3.apps.googleusercontent.com"
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "tjA0JQTXNiz_29DLW45EK8MR"
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 )
 
 SITE_ID = 2
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL= 'dashboard'
 # HOME_REDIRECT_URL= 'home'
--- Cryptotrade/Cryptotrade/urls.py
+++ Cryptotrade/Cryptotrade/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
+    path('oauth2/', include('social_django.urls', namespace='social')),
     path('', include('bitcotrade.urls')),
+    path('admin/', admin.site.urls),
 ]
